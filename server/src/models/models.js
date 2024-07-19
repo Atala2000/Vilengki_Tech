220db0efaf73362f9152861b14845713eacad04a
Atala2000 <atalakidi@gmail.com>
2024-07-19 18:45:30 +0300

    chore: Update database connection URL in models.js

--- server/src/models/models.js
+++ server/src/models/models.js
@@ -1,41 +1,41 @@
-import {Sequelize, DataTypes}  from 'sequelize'
+import { Sequelize, DataTypes } from 'sequelize'
 
 const sequelize = new Sequelize(
-    "mysql://erick:Watchdogsroot@2@localhost:3306/vilengki"
+    "mysql://kiddie:StrongPass123!@localhost:33061/vilengki"
 );
 
 const User = sequelize.define("User", {
-    fullname: {type: DataTypes.STRING, allowNull: false, unique:true},
-    useremail: {type: DataTypes.STRING, allowNull:false, unique: true},
-    userpassword: {type: DataTypes.STRING, allowNull:false, unique:true},
-    phone: {type: DataTypes.BIGINT, allowNull:false, unique:true},
-    county: {type: DataTypes.STRING, allowNull:false},
-    prepaidBalance: {type: DataTypes.FLOAT, allowNull:false, defaultValue : 0},
-    waterUsage: {type: DataTypes.FLOAT, allowNull:false, defaultValue: 0}
+    fullname: { type: DataTypes.STRING, allowNull: false, unique: true },
+    useremail: { type: DataTypes.STRING, allowNull: false, unique: true },
+    userpassword: { type: DataTypes.STRING, allowNull: false, unique: true },
+    phone: { type: DataTypes.BIGINT, allowNull: false, unique: true },
+    county: { type: DataTypes.STRING, allowNull: false },
+    prepaidBalance: { type: DataTypes.FLOAT, allowNull: false, defaultValue: 0 },
+    waterUsage: { type: DataTypes.FLOAT, allowNull: false, defaultValue: 0 }
 });
 
 const Admin = sequelize.define("Admin", {
-    fullname: {type: DataTypes.STRING, allowNull:false, unique:true},
-    adminemail: {type: DataTypes.STRING, allowNull:false, unique:true},
-    adminpassword: {type: DataTypes.STRING, allowNull:false, unique:true}
+    fullname: { type: DataTypes.STRING, allowNull: false, unique: true },
+    adminemail: { type: DataTypes.STRING, allowNull: false, unique: true },
+    adminpassword: { type: DataTypes.STRING, allowNull: false, unique: true }
 })
 
 const PaymentHistory = sequelize.define("PaymentHistory", {
-    fullname: {type: DataTypes.STRING, allowNull:false},
-    useremail: {type: DataTypes.STRING, allowNull:false},
-    timeCreated: {type: DataTypes.DATE, allowNull:false}
+    fullname: { type: DataTypes.STRING, allowNull: false },
+    useremail: { type: DataTypes.STRING, allowNull: false },
+    timeCreated: { type: DataTypes.DATE, allowNull: false }
 });
 
 const WaterPrice = sequelize.define("WaterPrice", {
-    price: {type: DataTypes.FLOAT, allowNull:false}
+    price: { type: DataTypes.FLOAT, allowNull: false }
 });
 
 
 (async () => {
     await sequelize.sync(); // Sync all defined models to the database
     console.log('All models were synchronized successfully.');
-    
-  }
-  )();
 
-export {User, PaymentHistory, WaterPrice, Admin}
+}
+)();
+
+export { User, PaymentHistory, WaterPrice, Admin }
