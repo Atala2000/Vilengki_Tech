b8e8f730f30cbf932e63a8a5bed24630c30c789a
Atala2000 <atalakidi@gmail.com>
2024-07-19 17:05:43 +0300

    feat: Add lazy loading for images in Payment component

--- client/vilengki_tech/src/components/Admin/Payment.jsx
+++ client/vilengki_tech/src/components/Admin/Payment.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
+
+const PaymentList = () => {
+    const [payments, setPayments] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const fetchPayments = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/api/payments");
+            const data = await response.json();
+            setPayments(data);
+            setLoading(false);
+
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+
+        fetchPayments();
+    })
+}
+
+
 const Payment = () => {
     return (
         <div>
-            <h1>Payment History</h1>
+            <h1>Payment List</h1>
+            <PaymentList />
         </div>
-    );
+    )
 }
-
 export default Payment;
